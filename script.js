3d4c45d66c46161c57462720460dffdafd9a826e
Denis Solodkovskiy <solodkovskiy@gmail.com>
2021-02-09 23:35:32 +0300

    changed script

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
     });
     window.addEventListener('touchstart', e => {
 
-        spotlightSize = 'transparent 130px, rgba(0, 0, 0, 0.8) 150px)';
+        spotlightSize = 'transparent 130px, rgba(0, 0, 0) 150px)';
 
         updateSpotlight(e);
 
@@ -48,9 +48,6 @@
 
     function getTimeRemaining(startTime) {
         let t = Date.parse(new Date()) - Date.parse(startTime),
-            seconds = Math.floor((t/1000) % 60),
-            minutes = Math.floor((t/60000) % 60),
-            hours = Math.floor((t/3.6e+6) % 24),
             years = Math.floor(t/3.154e+10);
             months = Math.floor((t/2.628e+9) % 12),
             days = Math.floor( ((t - (years * 3.154e+10)) - (months * 2.628e+9)) / 8.64e+7 );
@@ -59,10 +56,7 @@
                 "total" : t,
                 "years" : years,
                 "months" : months,
-                "days" : days,
-                "hours" : hours,
-                "minutes" : minutes,
-                "seconds" : seconds
+                "days" : days
             };
     }
 
@@ -71,19 +65,13 @@
             years = timer.querySelector(".years"),
             months = timer.querySelector(".months"),
             days = timer.querySelector(".days"),
-            hours = timer.querySelector(".hours"),
-            minutes = timer.querySelector(".minutes"),
-            seconds = timer.querySelector(".seconds"),
-            timeInterval = setInterval(updateClock, 1000);
+            timeInterval = setInterval(updateClock, 2,7778e-7);
 
             function updateClock() {
                 let t = getTimeRemaining(startTime);
                 years.textContent = t.years;
                 months.textContent = t.months;
                 days.textContent = t.days;
-                hours.textContent = t.hours;
-                minutes.textContent = t.minutes;
-                seconds.textContent = t.seconds;
                 }
             }
 
